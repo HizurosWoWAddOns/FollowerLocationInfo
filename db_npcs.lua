2a6f0fc750719e47358405fa3239120d29c0853c
Hizuro <hizuro@gmx.net>
2014-11-16 22:11:06 +0100

    added some npc names to db_npcs.lua

--- db_npcs.lua
+++ db_npcs.lua
@@ -1,23 +1,93 @@
 
 local addon, ns = ...;
 
-ns.npcs = {
-	
-};
+ns.npcs = {};
 
--- if LOCALE_deDE then end
+ns.npcs[73877] = "Jarrod Hamby";
+ns.npcs[75710] = "Hansel Heavyhands";
+ns.npcs[75884] = "Rulkan";
+ns.npcs[76204] = "Fiona";
+ns.npcs[76748] = "Shelly Hamby";
+ns.npcs[77014] = "Bruto";
+ns.npcs[77031] = "Ahm";
+ns.npcs[79322] = "Gladiator Akaani";
+ns.npcs[79966] = "Lost Packmule";
+ns.npcs[80073] = "Exarch Maladaar";
+ns.npcs[80078] = "Exarch Akama";
+ns.npcs[80163] = "Ken Loggin";
+ns.npcs[80672] = "Magister Serena";
+ns.npcs[80727] = "Rangari Arepheon";
+ns.npcs[81076] = "Rangari Jonaa";
+ns.npcs[81588] = "Thaelin Darkanvil";
+ns.npcs[81751] = "Hansel Heavyhands";
+ns.npcs[81772] = "Rangari Kaalya";
+ns.npcs[85119] = "Glirin";
+ns.npcs[88195] = "Hulda Shadowblade";
 
--- if LOCALE_esES or LOCALE_esMX then end
+if LOCALE_itIT then
+	ns.npcs[73877] = "Jarrod Hamby";
+	ns.npcs[75710] = "Hansel Maniforti";
+	ns.npcs[75884] = "Rulkan";
+	ns.npcs[76204] = "Fiona";
+	ns.npcs[76748] = "Shelly Hamby";
+	ns.npcs[77031] = "Ahm";
+	ns.npcs[79966] = "Mulo da Soma Perduto";
+	ns.npcs[80073] = "Esarca Maladaar";
+	ns.npcs[80078] = "Esarca Akama";
+	ns.npcs[80163] = "Ken Seghetti";
+	ns.npcs[80727] = "Guardaselve Arepheon";
+	ns.npcs[81588] = "Thaelin Forgiacupa";
+	ns.npcs[81751] = "Hansel Maniforti";
+	ns.npcs[81772] = "Guardaselve Kaalya";
+end
 
--- if LOCALE_frFR then end
+if LOCALE_ruRU then
+	ns.npcs[73877] = "Джаррод Хэмби";
+	ns.npcs[75884] = "Рулкан";
+	ns.npcs[76204] = "Фиона";
+	ns.npcs[76748] = "Шелли Хэмби";
+	ns.npcs[77031] = "Ахм";
+	ns.npcs[79966] = "Потерявшийся вьючный мул";
+	ns.npcs[80073] = "Экзарх Маладаар";
+	ns.npcs[80078] = "Экзарх Акама";
+	ns.npcs[80163] = "Кен Логгин";
+	ns.npcs[80672] = "Магистр Серена";
+	ns.npcs[80727] = "Рангари Арефеон";
+	ns.npcs[81076] = "Рангари Йонаа";
+	ns.npcs[81588] = "Телин Темная Наковальня";
+	ns.npcs[85119] = "Глирин";
+end
 
--- if LOCALE_itIT then end
+if LOCALE_deDE then
+	ns.npcs[76204] = "Fiona";
+	ns.npcs[77031] = "Ahm";
+	ns.npcs[79966] = "Verirrter Packesel";
+	ns.npcs[80073] = "Exarch Maladaar";
+	ns.npcs[80078] = "Exarch Akama";
+	ns.npcs[80163] = "Ken Kerbe";
+	ns.npcs[80727] = "Rangari Arepheon";
+	ns.npcs[81772] = "Rangari Kaalya";
+	ns.npcs[85119] = "Glirin";
+end
+
+if LOCALE_frFR then
+	ns.npcs[76204] = "Fiona";
+	ns.npcs[79966] = "Mule de bât perdue";
+	ns.npcs[80078] = "Exarque Akama";
+	ns.npcs[80163] = "Ken Loggin";
+end
+
+if LOCALE_ptBR then
+	ns.npcs[77014] = "Brutus";
+	ns.npcs[77031] = "Ahm";
+	ns.npcs[81772] = "Rangari Kaalya";
+end
+
+if LOCALE_esES or LOCALE_esMX then
+	ns.npcs[80078] = "Exarca Akama";
+end
+
 
 -- if LOCALE_koKR then end
-
--- if LOCALE_ptBR then end
-
--- if LOCALE_ruRU then end
-
 -- if LOCALE_zhCN or LOCALE_zhTW then end
 
