6fc192b524dec9078177e102131e9871b5c10f02
Hizuro <hizuro@gmx.net>
2014-11-27 08:09:03 +0100

    added description for goldmane (id:170)

--- followers/170/description.lua
+++ followers/170/description.lua
@@ -5,12 +5,15 @@ local _, ns = ...;
 
 ns.addFollower(
 	170,   -- follower id
-	false, -- neutral?
+	true, -- neutral?
 	{ -- A
 		zone = 950,
-	},
-	{ -- H
-		zone = 950,
+		{"pos", {950, 40.4, 76.2}},
+		{"quest", {950, 80083, 40.4, 76.2}},
+		{"desc", {
+			enUS = "Kill Bolkar the Cruel and loot the key for Goldmane's cage. Than open the cage and Goldmane will offer you his followship.",
+			deDE = "Töte 'Bolkar der Grausame' und erbeute den Schlüssel zu Goldmähne's Käfig. Dann öffne den Käfig und Goldmähne wird seine Gefolgschaft anbieten."
+		}}
 	}
 );
 --X
