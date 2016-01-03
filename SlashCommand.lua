11ed67118dd1d672a72133b8c6f5c6d079ab37aa
Hizuro <hizuro@gmx.net>
2016-01-03 14:46:45 +0100

    update SlashCommand.lua

--- SlashCommand.lua
+++ SlashCommand.lua
@@ -1,61 +1,73 @@
 
+local addon, ns = ...;
+
+local order,L = {
+	"journal",
+	--"ids",
+	"minimap",
+	"reset"
+};
+
+local cmdList = {
+	journal = {
+		desc = "Show/Hide journal frame",
+		func = function() FollowerLocationInfo_ToggleJournal() end
+	},
+	--[[
+	ids = {
+		desc = "Show/Hide followerID in journal",
+		func = function() FollowerLocationInfo_ToggleIDs() end
+	},
+	--]]
+	minimap = {
+		desc = "Show/Hide minimap button",
+		func = function() FollowerLocationInfo_MinimapButton() end
+	},
+	reset = {
+		desc = "Reset addon settings |cffff6666and automaticaly reloading the UI!|r",
+		func = function()
+			--FollowerLocationInfoDB = nil;
+			ReloadUI();
+		end
+	},
+};
+
+local _print = function(...)
+	local e = {"|cff00ff00 |||cffffff00",...};
+	if #e>2 then
+		tinsert(e,3,"|cff00ff00|||r");
+	else
+		e[1] = "|cff00ff00 |||cff44ffff";
+		tinsert(e,"|r");
+	end
+	print(unpack(e));
+end
+
 SlashCmdList["FOLLOWERLOCATIONINFO"] = function(cmd)
 	local cmd, arg = strsplit(" ", cmd, 2)
-	local _print = function(...) print("|cffff4444FLI|r", "|cff44aaff", ..., "|r"); end
+	L = FollowerLocationInfoData.Locale;
 
-	_print("Sorry. Currently, the chat commands are disabled.");
-	_print("But it will come back...");
-
-	--[=[
-	cmd = cmd:lower()
-	if (cmd=="toggle") then
-		FollowerLocationInfo_Toggle();
-	elseif (cmd=="collected") then
-		FollowerLocationInfo_ToggleCollected();
-	elseif (cmd=="ids") then
-		FollowerLocationInfo_ToggleIDs();
-	--elseif (cmd=="missing") then
-		--FollowerLocationInfo_PrintMissingData();
-	elseif (cmd=="minimap") then
-		FollowerLocationInfo_MinimapButton();
-	elseif (cmd=="list") then
-		FollowerLocationInfo_ToggleList();
-	elseif (cmd=="reset") then
-		FollowerLocationInfo_ResetConfig();
-	--elseif (cmd=="resetscale") then
-		--FollowerLocationInfo_ResetScale();
-	--elseif (cmd=="resetframe") then
-		--local f=FollowerLocationInfoFrame;
-		--f:SetUserPlaced(false);
-		--f:ClearAllPoints();
-		--f:SetPoint("LEFT",300,0);
-		--f:SetUserPlaced(true);
-	elseif (cmd=="genbasics") and (ns.generate_basics) then
-		ns.generate_basics();
-	elseif (cmd=="collectlocales") then
-		FollowerLocationInfo_Collector();
-	elseif (cmd=="delcollectedlocales") then
-		FLI_tmpDB = {};
+	if cmdList[cmd] then
+		cmdList[cmd].func();
 	else
-		_print(L["Commandline options for %s"]:format(addon));
-		_print(L["Usage: /fli <command>"]);
-		_print("      "..L["or /followerlocationinfo <command>"]);
-		_print(L["Commands:"]);
-		_print("  toggle = "     .. L["Show/Hide frame"]);
-		_print("  collected = "  .. L["Show/Hide collected followers"]);
-		_print("  ids = "        .. L["Show/Hide follower ids"]);
-		_print("  list = "       .. L["Show/Hide follower list"]);
-		_print("  minimap = "    .. L["Show/Hide minimap button"]);
-		_print("  reset =    "   .. L["Reset addon settings"]);
-		_print("  resetframe = "  .. L["Reset frame position"]);
-		--_print("  resetscale = " .. L["Reset window scaling"]);
-		_print("~ development commands ~");
-		--_print("  missing = "    .. L["Print missing data (follower and npc id's)"]);
-		_print("  collectlocales = "    .. L["Collects localized follower names from one faction. It is recommented to use it on both factions. The character must be level 90 or higher."]);
-		_print("  delcollectedlocales = "..L["Deletes collected localized follower names"]);
+		ns.print(L["Chat command usage"]);
+		_print("/followerlocationinfo "..L["<command>"]);
+		_print(L["or"].." /fli "..L["<command>"]);
+		ns.print(L["Commands"]);
+		for _, key in ipairs(order)do
+			if cmdList[key] then
+				_print(key,L[cmdList[key].desc]);
+			end
+		end
 	end
-	--]=]
 end
 
 SLASH_FOLLOWERLOCATIONINFO1 = "/fli";
 SLASH_FOLLOWERLOCATIONINFO2 = "/followerlocationinfo";
+
+--[=[
+L["Show/Hide journal frame"]
+L["Show/Hide followerID in journal"]
+L["Show/Hide minimap button"]
+L["Reset addon settings |cffff6666and automaticaly reloading the UI!|r"]
+--]=]
